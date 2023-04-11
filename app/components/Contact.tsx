ca4049150e7214a16efc5409bfcff9c5e7170bab
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-04-11 15:09:18 -0300

    feat(contact): improve contact design

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,14 +1,17 @@
 'use client'
 
-import { FormEvent, useRef } from 'react'
+import { FormEvent, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
 import classNames from 'classnames'
-import Paper from './Paper'
+import { FiSend } from 'react-icons/fi'
+import { HiOutlineMail } from 'react-icons/hi'
+import { FaWhatsapp, FaLinkedinIn } from 'react-icons/fa'
 import TitleHeader from './TitleHeader'
-import Paragraph from './Paragraph'
+import Paper from './Paper'
 
 export default function Contact() {
   const form = useRef<any>(null)
+  const [showAlert, setShowAlert] = useState(false)
 
   const sendEmail = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -23,6 +26,7 @@ export default function Contact() {
       .then(
         (result) => {
           console.log(result.text)
+          setShowAlert(true)
           form.current.reset()
         },
         (error) => {
@@ -31,21 +35,79 @@ export default function Contact() {
       )
   }
 
-  const contactContainerClass = classNames('flex', 'my-auto', 'gap-4')
+  const contactContainerClass = classNames(
+    'flex',
+    'flex-col',
+    'items-center',
+    'my-auto',
+    'gap-4'
+  )
 
-  const formClass = classNames('flex', 'flex-col', 'w-1/2')
+  const formClass = classNames(
+    'flex',
+    'flex-col',
+    'w-3/4',
+    'bg-bcg2',
+    'p-4',
+    'rounded'
+  )
 
-  const textInputsClass = classNames('text-bcg1', 'bg-frg1', 'rounded', 'mb-2')
+  const alertMessageClass = classNames(
+    'flex',
+    'items-center',
+    'w-full',
+    'rounded',
+    'border',
+    'p-2',
+    'text-green',
+    'border-green',
+    'bg-green/[.06]',
+    'mb-4',
+    showAlert ? 'block' : 'hidden'
+  )
 
-  const buttonClass = classNames()
+  const headingLineCLas = classNames('text-frg2', 'font-bold', 'text-2xl', 'mb-4')
+
+  const textInputsClass = classNames(
+    'text-bcg1',
+    'bg-frg1',
+    'rounded',
+    'mb-2',
+    'w-full'
+  )
+
+  const buttonClass = classNames(
+    'flex',
+    'items-center',
+    'gap-2',
+    'w-fit',
+    'rounded',
+    'border',
+    'p-2',
+    'text-green',
+    'border-green',
+    'hover:bg-green/[.06]'
+  )
+
+  const contactLineClass = classNames(
+'flex','items-center', 'gap-2'
+  )
+
+  const contactIconClass = classNames(
+    'text-green',
+    'text-xl'
+  )
 
   return (
     <Paper>
       <TitleHeader>Contato</TitleHeader>
       <div className={contactContainerClass}>
         <form ref={form} onSubmit={sendEmail} className={formClass}>
+          <p className={alertMessageClass}>Mensagem enviada com sucesso!</p>
+          <h3 className={headingLineCLas}>Envie uma mensagem</h3>
           <label htmlFor="name">
             Nome
+            <br />
             <input
               id="name"
               type="text"
@@ -56,6 +118,7 @@ export default function Contact() {
 
           <label htmlFor="email">
             Seu e-mail
+            <br />
             <input
               id="email"
               type="email"
@@ -66,15 +129,28 @@ export default function Contact() {
 
           <label htmlFor="message">
             Mensagem
-            <textarea id="message" name="message" className={textInputsClass} />
+            <br />
+            <textarea id="message" name="message" rows={5} className={textInputsClass} />
           </label>
-          <input type="submit" value="Enviar" className={buttonClass} />
+          <button type="submit" value="Enviar" className={buttonClass}>
+            Enviar
+            <FiSend />
+          </button>
         </form>
         <div>
-          <h3>Ou faça contato direto</h3>
-          <Paragraph>nicolas.brandaor@gmail.com</Paragraph>
-          <Paragraph>+55 37 9 9112 4140</Paragraph>
-          <Paragraph>in/nicolasbradao</Paragraph>
+          <h3 className={headingLineCLas}>Ou faça contato direto</h3>
+          <p className={contactLineClass}>
+            <HiOutlineMail className={contactIconClass}/>
+            nicolas.brandaor@gmail.com
+          </p>
+          <p className={contactLineClass}>
+            <FaWhatsapp className={contactIconClass}/>
+            +55 37 9 9112 4140
+          </p>
+          <p className={contactLineClass}>
+            <FaLinkedinIn className={contactIconClass}/>
+            in/nicolasbradao
+          </p>
         </div>
       </div>
     </Paper>
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 
 export default function Home() {
-  const mainClass = classNames('mx-auto', 'w-[50rem]')
+  const mainClass = classNames('mx-auto', 'w-[50rem]', 'border')
   return (
     <main className={mainClass}>
       <Hero />
