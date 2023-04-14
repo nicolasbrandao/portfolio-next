39e0adf1b11d3fe67cfbefcf1ddcc3bb72d2ce70
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-04-14 18:05:45 -0300

    feat: favicon package

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import classNames from 'classnames'
 import { Inter } from 'next/font/google'
 import { ReactNode } from 'react'
+import Head from 'next/head'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import PreLoader from './components/PreLoader'
@@ -18,6 +19,41 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 
   return (
     <html lang="pt-br" className="dark">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="../public/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="../public/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="../public/favicon-16x16.png"
+        />
+        <link rel="manifest" href="../public/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="../public/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="N&iacute;colas Brand&atilde;o - Software Engineer"
+        />
+        <meta
+          name="application-name"
+          content="N&iacute;colas Brand&atilde;o - Software Engineer"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body className={bodyClass}>
         <Navbar />
         {children}
