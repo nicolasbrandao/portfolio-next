61dbc34b2c6e59458abcfecf032aed0a19775292
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-04-14 18:52:51 -0300

    fix: favicon metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import classNames from 'classnames'
 import { Inter } from 'next/font/google'
 import { ReactNode } from 'react'
-import Head from 'next/head'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import PreLoader from './components/PreLoader'
@@ -12,6 +11,11 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Nícolas Brandão - Software Engineer',
   description: 'Página de portfólio pessoal de Nícolas Brandão',
+  colorScheme: 'dark',
+  icons: {
+    icon: { url: '/favicon/favicon.ico' },
+    shortcut: { url: '/favicon/favicon.ico' },
+  },
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
@@ -19,41 +23,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 
   return (
     <html lang="pt-br" className="dark">
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <meta
-          name="apple-mobile-web-app-title"
-          content="N&iacute;colas Brand&atilde;o - Software Engineer"
-        />
-        <meta
-          name="application-name"
-          content="N&iacute;colas Brand&atilde;o - Software Engineer"
-        />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
       <body className={bodyClass}>
         <Navbar />
         {children}
