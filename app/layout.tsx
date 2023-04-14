eca6c93ef162071a1a65b2f9c75ce11ecd82384f
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-04-14 18:21:58 -0300

    fix: favicon package import

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,26 +23,22 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
+          href="/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon/favicon-32x32.png"
+          href="/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon/favicon-16x16.png"
+          href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta
           name="apple-mobile-web-app-title"
           content="N&iacute;colas Brand&atilde;o - Software Engineer"
