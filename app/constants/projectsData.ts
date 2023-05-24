cb1f31310a4a8b594e2238891a504bd3a7a65fc7
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-05-24 20:25:25 -0300

    fea: add new project (fakestore)

--- app/constants/projectsData.ts
+++ app/constants/projectsData.ts
@@ -1,8 +1,19 @@
 import { Project } from '@/types'
+import FakeStoreThumbnail from '../../public/projects/fakestore.png'
 import GamerShelfThumbnail from '../../public/projects/gamer-shelf.png'
 import PortfolioThumbnail from '../../public/projects/portfolio.png'
 
 const projectsData: Project[] = [
+  {
+    id: 'fake-store',
+    image: FakeStoreThumbnail,
+    repoURL: 'https://github.com/nicolasbrandao/fake-store',
+    deployURL: 'https://next-fakestore.vercel.app/',
+    title: 'FakeStore',
+    description:
+      'Projeto que utiliza a Fake Store API para criar um web app e-commerce onde o usuário pode navegar por produtos e adicioná-los ao carrinho',
+    technologies: 'Next appDir ContextAPI ChakraUI TypeScript Tailwind',
+  },
   {
     id: 'gamer-shelf',
     image: GamerShelfThumbnail,
