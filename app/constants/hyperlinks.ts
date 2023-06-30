2be943a59645783893d8cc45443ece86fd6328bc
nicolasbrandao <nicolas.brandaor@gmail.com>
2023-06-30 15:52:43 -0300

    fea: add resume as gdrive link

--- app/constants/hyperlinks.ts
+++ app/constants/hyperlinks.ts
@@ -5,7 +5,8 @@ const linksURLs = {
   notion:
     'https://frill-word-deb.notion.site/05e98f1767054c43a2cea6118e331bcb?v=a47a1074574940c7a0a7091ccdd706de',
   github: 'https://github.com/nicolasbrandao',
-  resume: '/pdf/CV_NICOLAS_BRANDAO.pdf',
+  resume:
+    'https://docs.google.com/document/d/1BPBNfYRSCfNHiUCyS3ePqp__-OZMXCnG/edit?usp=sharing&ouid=112692960599822454672&rtpof=true&sd=true',
   repo: 'https://github.com/nicolasbrandao/portfolio-next',
 }
 
